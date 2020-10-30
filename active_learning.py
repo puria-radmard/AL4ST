996ea983e43e0bcbd1be02d0ba147f427835218d
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-10-30 21:17:59 +0000

    structure change, added BALD and window method

--- active_learning.py
+++ active_learning.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1491505b85d9bb1048b9cf45ccffc2db97b7172c3d372c784705e5c477b2e70
-size 11259
+oid sha256:f41f0726513a5c08f83ed01cea312a334cfd43a046563dfcc4f03d97f71175ff
+size 16479
--- model.py
+++ model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f773b6e89a55c9d7275644b2321df7382a64fc7c123b09e65f9fd73c2ff2a16
-size 6385
+oid sha256:6c38c6b3d5e996aed87c0dab59f930e08d4c63215c528badcdc0fe9e0f6f938f
+size 6391
