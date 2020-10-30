33416199921bd210882f1d0f7d9836978fb87358
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-10-30 21:17:59 +0000

    structure change, added BALD and window method

--- al_train.py
+++ al_train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:247d159a52c80564065fd32c5b887e29320ffaf8b20b2cba022ec49d2554828b
-size 11330
+oid sha256:0aac84f645f9661a6cd886bc816aa0c2368ffe3c965caadab0a668c1824efb91
+size 16252
