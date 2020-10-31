87a7264311b27a622f2e2f82781170f492b39c13
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-10-31 17:13:26 +0000

    running 4 baselines

--- al_train.py
+++ al_train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0aac84f645f9661a6cd886bc816aa0c2368ffe3c965caadab0a668c1824efb91
-size 16252
+oid sha256:518771921863412c0da77c1d8a4f2d88d5645da17b3e4715d5ebcf5b9624a1f3
+size 16291
