f5f0f9018a2cfa611e2f47abacf457efbcee3296
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-11-06 10:54:35 +0000

    added tests

--- al_train.py
+++ al_train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:026d4c0999a33757369faf813150034deb5ac6face6904cf37ad9ec80f48bacf
-size 16268
+oid sha256:56c988cdbbdf39672ad472c0f84aea0fe335075c2ed49cbff10bda27ae483009
+size 16267
