91eb8b8dc58e449b794d75db3144c5b41e352a01
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-11-17 01:38:14 +0000

    fixed return

--- data_general.py
+++ data_general.py
@@ -45,6 +45,8 @@ def construct_data_dictionary(sentence_df: pd.DataFrame, token_col: str, label_c
         "entityMentions": label_list
     }
 
+    return data_dict
+
 
 def make_dataset_jsons(file_mappings: Dict[str, str], col_names: List[str], token_col: str, label_col: str):
 
