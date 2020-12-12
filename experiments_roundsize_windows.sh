20cd1fc80b1e047594c9faf9b4073d37ff05b934
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-12-12 19:06:02 +0000

    added round_size experiments

---
+++ experiments_roundsize_windows.sh
@@ -0,0 +1,4 @@
+python train.py -W 5 -A lc -T 1 -alpha 1 --beta 0 -R 20000 -I 0.01
+python train.py -W 5 -A lc -T 1 -alpha 1 --beta 0 -R 50000 -I 0.01
+python train.py -W 5 -A lc -T 1 -alpha 1 --beta 0 -R 80000 -I 0.01
+python train.py -W 5 -A lc -T 1 -alpha 1 --beta 0 -R 100000 -I 0.01
