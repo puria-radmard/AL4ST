c471a8c3c93948bde6bc736a2fc1e1e86b63fed8
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-12-12 19:10:43 +0000

    added variable window size to experiment scripts

---
+++ experiments_roundsize_variable_windows.sh
@@ -0,0 +1,4 @@
+python train.py -W 2 7 -A lc -T 1 -alpha 1 --beta 0 -R 20000 -I 0.01
+python train.py -W 2 7 -A lc -T 1 -alpha 1 --beta 0 -R 50000 -I 0.01
+python train.py -W 2 7 -A lc -T 1 -alpha 1 --beta 0 -R 80000 -I 0.01
+python train.py -W 2 7 -A lc -T 1 -alpha 1 --beta 0 -R 100000 -I 0.01
