685c0f6d43ebceeb218748615ee5e3a6e8c8afec
pr450@cam.ac.uk <pr450@cam.ac.uk>
2020-12-15 14:43:20 +0000

    final final exps

--- experiments_final.sh
+++ experiments_final.sh
@@ -16,5 +16,7 @@ conda activate experiments && python train.py -W 3 7 -A lc -T 1 -alpha 0.7 --bet
 conda activate experiments && python train.py -W 3 7 -A lc -T 1 -alpha 0 --beta 0 -R 10000 -B 1 -I 0.01
 conda activate experiments && python train.py -W 3 7 -A lc -T 1 -alpha 0 --beta 0 -R 10000 -B 4 -I 0.01
 
-# Random baseline
+# Random and FS baseline leftover
 conda activate experiments && python train.py -W 3 7 -A lc -T 1 -alpha 1 --beta 0 -R 10000 -B 1 -I 0.01
+conda activate experiments && python train.py -W '-1' -A lc -T 1 -alpha 0 --beta 0 -R 10000 -B 1 -I 0.01
+conda activate experiments && python train.py -W '-1' -A lc -T 1 -alpha 0.7 --beta 0 -R 10000 -B 1 -I 0.01
