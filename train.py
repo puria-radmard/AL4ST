f3e12edad24aadaaa21490535f76939bc14f1025
pr450@cam.ac.uk <pr450@cam.ac.uk>
2021-01-23 17:13:07 +0000

    minor change

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def parse_args():
     )
     parser.add_argument(
         "-propagation", "--allow_propogation", type=int,
-        help="1 or 0 please, allow label propagation", default=0
+        help="1 or 0 please, allow label propagation"
     )
     parser.add_argument(
         "--beta", type=float, help="Weight (should be in [0,1]) that self-supervised losses are multiplied by",
